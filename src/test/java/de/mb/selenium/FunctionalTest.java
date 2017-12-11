b2af49a4979664d517389fce34f466562b2ebcd5
devopsevd <devopsevd@users.noreply.github.com>
2017-12-11 20:56:33 +0530

    Update and rename GoogleStartPageTest.java to FunctionalTest.java

--- src/test/java/de/mb/selenium/FunctionalTest.java
+++ src/test/java/de/mb/selenium/FunctionalTest.java
@@ -1,5 +1,23 @@
 package de.mb.selenium;
 
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.hpe.alm.octane.OctaneCucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(OctaneCucumber.class)
+@CucumberOptions(plugin={"junit:junitResult.xml"},
+                features= {"src/test/java/de/mb/selenium/"})
+
+public class FunctionalTest {
+
+}
+
+
+/*
+package de.mb.selenium;
+
 import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
@@ -45,3 +63,4 @@ public class GoogleStartPageTest {
 
 
 }
+*/
